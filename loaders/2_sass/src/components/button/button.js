ffe36e4ff9de0da4f0f0f817c4d4a5c8b6434ac1
ולדימיר יליסבטסקי <elisavet_v@mac.org.il>
2021-11-30 10:09:25 +0200

    css and sass

---
+++ loaders/2_sass/src/components/button/button.js
@@ -0,0 +1,18 @@
+import './button.scss'
+
+class Button {
+    render() {
+        const button = document.createElement('button');
+        button.innerHTML = 'my button';
+        button.classList.add('button-style')
+        const body = document.querySelector('body')
+        button.onclick = () => {
+            const p = document.createElement('p')
+            p.innerHTML = 'run '
+            p.classList.add('p-style')
+            body.appendChild(p)
+        }
+        body.appendChild(button)
+    }
+}
+export default Button; 
